3e844416fcccacf5b3dfec4f0b3e982098cbbe84
Reid Nilson <reid.nilson@gmail.com>
2015-10-03 12:30:36 -0600

    Read MySQL passwords from encrypted data bag.

---
+++ cookbooks/mysql/recipes/server.rb
@@ -0,0 +1,7 @@
+# Customization: get passwords from encrypted data bag
+secrets = Chef::EncryptedDataBagItem.load("secrets", "mysql")
+if secrets && mysql_passwords = secrets[node.chef_environment] 
+  node['mysql']['server_root_password'] = mysql_passwords['root']
+  node['mysql']['server_debian_password'] = mysql_passwords['debian']
+  node['mysql']['server_repl_password'] = mysql_passwords['repl']
+end
